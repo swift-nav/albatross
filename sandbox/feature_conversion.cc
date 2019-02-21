ae408c8557920f86b727be43ba7f37efac4dfbd3
kleeman <akleeman@gmail.com>
2019-02-21 08:52:28 -0800

    Avoid Inheritance

--- sandbox/feature_conversion.cc
+++ sandbox/feature_conversion.cc
@@ -21,13 +21,6 @@ public:
     derived().fit_(feature);
   }
 
-  template <typename FeatureType, typename OtherType>
-  typename std::enable_if<has_possible_fit_impl<ModelType, FeatureType>::value, void>::type
-  fit(const OtherType &other) {
-    FeatureType feature = derived().convert_(other);
-    fit(feature);
-  }
-
   /*
    * CRTP Helpers
    */
@@ -37,34 +30,34 @@ public:
   }
 };
 
-template <typename Derived>
-class Middle : public ModelBase<Middle<Derived>> {
-public:
+void middle_fit(const X feature) {
+  std::cout << "middle_fit(X) : " << feature.value << std::endl;
+}
 
-  template <typename T>
-  X convert_(const T &feature) {
-    return derived().convert_(feature);
-  }
+class Middle {
+public:
 
   void fit_(const X &feature) {
     std::cout << "Middle.fit_(X) : " << feature.value << std::endl;
   }
 
-  /*
-   * CRTP Helpers
-   */
-  Derived &derived() { return *static_cast<Derived *>(this); }
-  const Derived &derived() const {
-    return *static_cast<const Derived *>(this);
-  }
 };
 
-class Adapted : public Middle<Adapted> {
+class Adapted : public ModelBase<Adapted> {
 public:
 
-  X convert_(const Y &feature) const {
-    std::cout << "Adapted.convert_(Y) : " << feature.value << std::endl;
-    return {feature.value};
+  void fit_(const X &feature) {
+    std::cout << "Adapted.fit_(X) : " << feature.value << std::endl;
+    middle_fit(feature);
+  }
+
+  X convert_feature(const Y &y) {
+    return {y.value};
+  }
+
+  void fit_(const Y &feature) {
+    std::cout << "Adapted.fit_(Y) : " << feature.value << std::endl;
+    fit_(convert_feature(feature));
   }
 
 };
@@ -81,7 +74,7 @@ int main()
   m.fit(x);
 
   Y y = {2};
-  m.fit<X>(y);
+  m.fit(y);
   // I'd really like to be able to do:
   //  m.fit(y);
 
