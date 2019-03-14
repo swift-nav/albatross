2d6bd486d7034ed93ccc8ee559dd1be7907dcb33
kleeman <akleeman@gmail.com>
2019-03-14 15:31:03 -0700

    Switch EvaluationMetric in evaluate.h

--- albatross/evaluation/evaluate.h
+++ albatross/evaluation/evaluate.h
@@ -13,48 +13,60 @@
 #ifndef ALBATROSS_EVALUATE_H
 #define ALBATROSS_EVALUATE_H
 
-#include <functional>
-#include <map>
-#include <math.h>
-#include <memory>
-
-#include "core/dataset.h"
-#include "likelihood.h"
-
 namespace albatross {
 
 /*
- * Evaluation metrics are best kept in a separate namespace since
- * the compiler can get confused with the use of std::function
- * (which is used in the definition of an EvaluationMetric) and
- * overloaded functions.
+ * An EvaluationMetric is basically just a wrapper around a
+ * function which enforces a signature used in evaluation.  One
+ * alternative would be to use std::function.  Ie,
  *
- * https://stackoverflow.com/questions/30393285/stdfunction-fails-to-distinguish-overloaded-functions
+ *   template <typename RequiredPredictType>
+ *   using EvaluationMetric =
+ *         std::function<double(const RequiredPredictType &,
+ *                              const MarginalDistribution &);
+ *
+ * the problem with this is that when you pass the std::function
+ * object as an argument the RequiredPredictType cannot be
+ * inferred.
  */
-namespace evaluation_metrics {
+template <typename RequiredPredictType> struct EvaluationMetric {
+  virtual double operator()(const RequiredPredictType &,
+                            const MarginalDistribution &) const = 0;
+};
 
 static inline double root_mean_square_error(const Eigen::VectorXd &prediction,
-                                            const MarginalDistribution &truth) {
-  const Eigen::VectorXd error = prediction - truth.mean;
+                                            const Eigen::VectorXd &truth) {
+  const Eigen::VectorXd error = prediction - truth;
   double mse = error.dot(error) / static_cast<double>(error.size());
   return sqrt(mse);
 }
 
-/*
- * Takes output from a model (PredictionDistribution)
- * and the corresponding truth and uses them to compute the stddev.
- */
+struct RootMeanSquareError : public EvaluationMetric<Eigen::VectorXd> {
+  double operator()(const Eigen::VectorXd &prediction,
+                    const MarginalDistribution &truth) const override {
+    return root_mean_square_error(prediction, truth.mean);
+  }
+};
+
 static inline double standard_deviation(const Eigen::VectorXd &prediction,
-                                        const MarginalDistribution &truth) {
-  Eigen::VectorXd error = prediction - truth.mean;
+                                        const Eigen::VectorXd &truth) {
+  Eigen::VectorXd error = prediction - truth;
   const auto n_elements = static_cast<double>(error.size());
   const double mean_error = error.sum() / n_elements;
   error.array() -= mean_error;
   return std::sqrt(error.dot(error) / (n_elements - 1));
 }
 
+struct StandardDeviation : public EvaluationMetric<Eigen::VectorXd> {
+  double operator()(const Eigen::VectorXd &prediction,
+                    const MarginalDistribution &truth) const override {
+    return standard_deviation(prediction, truth.mean);
+  }
+};
+
 /*
- * Computes the negative log likelihood under the assumption that the predictive
+ * Computes the negative log likelihood under the assumption that the
+ * predictive
  * distribution is multivariate normal.
  */
 static inline double
@@ -68,7 +80,24 @@ negative_log_likelihood(const JointDistribution &prediction,
   return albatross::negative_log_likelihood(mean, covariance);
 }
 
-} // namespace evaluation_metrics
+static inline double
+negative_log_likelihood(const MarginalDistribution &prediction,
+                        const MarginalDistribution &truth) {
+  const Eigen::VectorXd mean = prediction.mean - truth.mean;
+  Eigen::VectorXd variance(prediction.covariance.diagonal());
+  if (truth.has_covariance()) {
+    variance += truth.covariance.diagonal();
+  }
+  return albatross::negative_log_likelihood(mean, variance.asDiagonal());
+}
+
+template <typename PredictType = JointDistribution>
+struct NegativeLogLikelihood : public EvaluationMetric<PredictType> {
+  double operator()(const PredictType &prediction,
+                    const MarginalDistribution &truth) const override {
+    return negative_log_likelihood(prediction, truth);
+  }
+};
 
 } // namespace albatross
 
