511a24edadf7b10aa3ae7ae95aee0059e5fc6fb3
Matt Woodward <matthew.woodward@swift-nav.com>
2019-05-28 14:48:14 +1000

    Include gzip headers as system

--- cmake/FindGzip-Hpp.cmake
+++ cmake/FindGzip-Hpp.cmake
@@ -1,7 +1,7 @@
 # Non-swift code that doesn't nicely export a library target, can't use the generic module
 if(EXISTS "${PROJECT_SOURCE_DIR}/third_party/gzip-hpp/CMakeLists.txt")
   add_library(gzip-hpp INTERFACE)
-  target_include_directories(gzip-hpp INTERFACE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/third_party/gzip-hpp/include>)
+  target_include_directories(gzip-hpp SYSTEM INTERFACE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/third_party/gzip-hpp/include>)
 else()
   if(Gzip-Hpp_FIND_REQUIRED)
     message(FATAL_ERROR "Could not find REQUIRED package Gzip-Hpp")
