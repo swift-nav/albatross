2f4a1d7b71c3d36df35d6dc72bf2de4b829f8361
Alex Kleeman <akleeman@gmail.com>
2019-08-04 09:25:17 -0700

    Scaling Function Fix (#148)
    * add scaling function tests
    
    * Fix scaling function products

--- tests/test_scaling_function.cc
+++ tests/test_scaling_function.cc
@@ -147,4 +147,64 @@ TEST(test_scaling_functions, test_inference) {
   // that we made scaled observations of it.
   EXPECT_LE(fabs(state_estimate.mean()[0] - attenuation), 1e-2);
 }
+
+class DummyCovariance : public CovarianceFunction<DummyCovariance> {
+public:
+  template <typename X, typename Y>
+  double _call_impl(const X &x, const Y &y) const {
+    return 0.;
+  }
+};
+
+/*
+ * This test make sure (and illustrates how) we can perform inference
+ * on the unknown attenuation constant of the material in the test
+ * case described above.
+ */
+TEST(test_scaling_functions, test_operations) {
+  double sigma = 0.01;
+
+  using TestScalingTerm = ScalingTerm<ObliquityScaling>;
+  TestScalingTerm scaling;
+
+  using DoubleNoise = IndependentNoise<double>;
+  DoubleNoise double_noise(sigma);
+
+  DummyCovariance dummy;
+
+  auto rhs_cov_func = double_noise * scaling + dummy;
+
+  double a = 0.;
+  double b = 1.;
+
+  struct X {};
+  struct Y {};
+
+  X x;
+  Y y;
+
+  EXPECT_EQ(rhs_cov_func(x, y), 0.);
+  EXPECT_EQ(rhs_cov_func(x, x), 0.);
+  EXPECT_EQ(rhs_cov_func(x, y), 0.);
+  EXPECT_EQ(rhs_cov_func(a, y), 0.);
+  EXPECT_EQ(rhs_cov_func(a, x), 0.);
+  EXPECT_EQ(rhs_cov_func(a, b), 0.);
+  EXPECT_GT(rhs_cov_func(a, a), 0.);
+
+  /*
+   * The scaling functions need to be defined differently
+   * for left hand side (LHS) and right hand side (RHS)
+   * products, so we test both.
+   */
+  auto lhs_cov_func = scaling * double_noise + dummy;
+
+  EXPECT_EQ(lhs_cov_func(x, y), 0.);
+  EXPECT_EQ(lhs_cov_func(x, x), 0.);
+  EXPECT_EQ(lhs_cov_func(x, y), 0.);
+  EXPECT_EQ(lhs_cov_func(a, y), 0.);
+  EXPECT_EQ(lhs_cov_func(a, x), 0.);
+  EXPECT_EQ(lhs_cov_func(a, b), 0.);
+  EXPECT_GT(lhs_cov_func(a, a), 0.);
+}
+
 } // namespace albatross
