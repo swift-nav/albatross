857059d23e39896bfcdc7625633978eaec0bc17f
kleeman <akleeman@gmail.com>
2020-01-31 15:39:27 -0800

    Modify callbacks to hold a ParameterStore instead of a model.
    Add a callback which stores the maximum likelihood.

--- include/albatross/src/samplers/callbacks.hpp
+++ include/albatross/src/samplers/callbacks.hpp
@@ -19,7 +19,8 @@ struct NullCallback {
   void operator()(std::size_t, const EnsembleSamplerState &){};
 };
 
-inline std::vector<std::string> get_columns(const ParameterStore &example) {
+inline std::vector<std::string>
+get_sampler_csv_columns(const ParameterStore &example) {
   std::vector<std::string> columns;
   columns.push_back("iteration");
   columns.push_back("log_probability");
@@ -29,19 +30,17 @@ inline std::vector<std::string> get_columns(const ParameterStore &example) {
   return columns;
 }
 
-template <typename ModelType>
-inline void
-write_ensemble_sampler_state(std::ostream &stream, const ModelType &model,
-                             const EnsembleSamplerState &ensemble,
-                             std::size_t iteration,
-                             const std::vector<std::string> &columns) {
+inline void write_ensemble_sampler_state(
+    std::ostream &stream, const ParameterStore &param_store,
+    const EnsembleSamplerState &ensemble, std::size_t iteration,
+    const std::vector<std::string> &columns) {
 
-  ModelType m(model);
   for (std::size_t i = 0; i < ensemble.size(); ++i) {
     std::map<std::string, std::string> row;
 
-    m.set_tunable_params_values(ensemble[i].params);
-    for (const auto &param : m.get_params()) {
+    const auto params =
+        set_tunable_params_values(ensemble[i].params, param_store);
+    for (const auto &param : params) {
       row[param.first] = std::to_string(param.second.value);
     }
     row["iteration"] = std::to_string(iteration);
@@ -51,42 +50,72 @@ write_ensemble_sampler_state(std::ostream &stream, const ModelType &model,
   }
 }
 
-template <typename ModelType> struct CsvWritingCallback {
+struct MaximumLikelihoodTrackingCallback {
 
-  CsvWritingCallback(const ModelType &model_,
+  MaximumLikelihoodTrackingCallback(const ParameterStore &param_store_,
+                                    std::shared_ptr<std::ostream> &stream_)
+      : param_store(param_store_), stream(stream_){};
+
+  MaximumLikelihoodTrackingCallback(const ParameterStore &param_store_,
+                                    std::shared_ptr<std::ostream> &&stream_)
+      : param_store(param_store_), stream(std::move(stream_)){};
+
+  void operator()(std::size_t iteration,
+                  const EnsembleSamplerState &ensembles) {
+    for (const auto &state : ensembles) {
+      if (state.log_prob > max_ll) {
+        max_ll = state.log_prob;
+        param_store = set_tunable_params_values(state.params, param_store);
+        (*stream) << "===================" << std::endl;
+        (*stream) << "Iteration: " << iteration << std::endl;
+        (*stream) << "LL: " << max_ll << std::endl;
+        (*stream) << pretty_params(param_store) << std::endl;
+      }
+    }
+  }
+
+  double max_ll = -HUGE_VAL;
+  ParameterStore param_store;
+  std::shared_ptr<std::ostream> stream;
+};
+
+struct CsvWritingCallback {
+
+  CsvWritingCallback(const ParameterStore &param_store_,
                      std::shared_ptr<std::ostream> &stream_)
-      : model(model_), stream(stream_){};
+      : param_store(param_store_), stream(stream_){};
 
-  CsvWritingCallback(const ModelType &model_,
+  CsvWritingCallback(const ParameterStore &param_store_,
                      std::shared_ptr<std::ostream> &&stream_)
-      : model(model_), stream(std::move(stream_)){};
+      : param_store(param_store_), stream(std::move(stream_)){};
 
   void operator()(std::size_t iteration,
                   const EnsembleSamplerState &ensembles) {
     if (iteration == 0) {
-      columns = get_columns(model.get_params());
+      columns = get_sampler_csv_columns(param_store);
       write_header(*stream, columns);
     }
-    write_ensemble_sampler_state(*stream, model, ensembles, iteration, columns);
+    write_ensemble_sampler_state(*stream, param_store, ensembles, iteration,
+                                 columns);
   }
 
-  ModelType model;
+  ParameterStore param_store;
   std::shared_ptr<std::ostream> stream;
   std::vector<std::string> columns;
 };
 
 template <typename ModelType>
-CsvWritingCallback<ModelType> get_csv_writing_callback(const ModelType &model,
-                                                       std::string path) {
-  return CsvWritingCallback<ModelType>(model,
-                                       std::make_shared<std::ofstream>(path));
+CsvWritingCallback get_csv_writing_callback(const ModelType &model,
+                                            std::string path) {
+  return CsvWritingCallback(model.get_params(),
+                            std::make_shared<std::ofstream>(path));
 }
 
 template <typename ModelType>
-CsvWritingCallback<ModelType>
+CsvWritingCallback
 get_csv_writing_callback(const ModelType &model,
                          std::shared_ptr<std::ostream> &stream) {
-  return CsvWritingCallback<ModelType>(model, stream);
+  return CsvWritingCallback(model.get_params(), stream);
 }
 
 } // namespace albatross
