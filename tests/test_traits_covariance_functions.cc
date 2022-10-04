993582d70d94ce72593c2acc96b14f14f467f755
kleeman <akleeman@gmail.com>
2022-10-04 10:19:41 -0700

    test with has_usable

--- tests/test_traits_covariance_functions.cc
+++ tests/test_traits_covariance_functions.cc
@@ -57,6 +57,16 @@ TEST(test_traits_covariance, test_has_valid_call_impl) {
   EXPECT_FALSE(bool(has_valid_call_impl<HasMultiple, Z, Z>::value));
 }
 
+TEST(test_traits_covariance, test_has_usable_call_impl) {
+  EXPECT_TRUE(bool(has_usable_call_impl<HasPublicCallImpl, X, Y>::value));
+  EXPECT_TRUE(bool(has_usable_call_impl<HasPublicCallImpl, Y, X>::value));
+  EXPECT_TRUE(bool(has_usable_call_impl<HasMultiple, X, X>::value));
+  EXPECT_TRUE(bool(has_usable_call_impl<HasMultiple, Y, Y>::value));
+  EXPECT_FALSE(bool(has_usable_call_impl<HasMultiple, Z, X>::value));
+  EXPECT_FALSE(bool(has_usable_call_impl<HasMultiple, Z, Y>::value));
+  EXPECT_FALSE(bool(has_usable_call_impl<HasMultiple, Z, Z>::value));
+}
+
 /*
  * Here we test to make sure we can identify situations where
  * _call_impl( has been defined but not necessarily properly.
