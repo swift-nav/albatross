56d73c6d333f2d136086c1e4130e8547ca5d4519
Matt Peddie <matt.peddie@swift-nav.com>
2022-12-09 13:37:25 +1000

    Add sanitizer support and sanitized CI target

---
+++ ci/run_thread_sanitizer_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+set -e
+
+run_tests() {
+    mkdir -p build
+    cd build
+    cmake -DENABLE_AUTOLINT=ON \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DCMAKE_C_COMPILER="$C_COMPILER" \
+          -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
+          -DSWIFT_SANITIZE_THREAD=ON \
+          ../
+    TSAN_OPTIONS="force_seq_cst_atomics=1 halt_on_error=1" \
+        make \
+        -j2 \
+        run_albatross_unit_tests \
+        run_inspection_example \
+        run_sinc_example \
+        run_temperature_example \
+        run_sampler_example
+    cd ..
+}
+
+run_tests
