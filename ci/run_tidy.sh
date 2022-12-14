a078f97a45dfd0fbacf2266ab248d829468c0599
Matt Peddie <matt.peddie@swift-nav.com>
2022-12-14 15:34:08 +1000

    Set up ratchet checks for clang-tidy warnings

--- ci/run_tidy.sh
+++ ci/run_tidy.sh
@@ -4,13 +4,12 @@ set -e
 run_tidy() {
     mkdir -p build
     cd build
-    cmake -DENABLE_AUTOLINT=ON \
-          -DCMAKE_BUILD_TYPE=Release \
+    cmake -DCMAKE_BUILD_TYPE=Release \
           -Dalbatross_ENABLE_CLANG_TIDY=ON \
           -DCMAKE_C_COMPILER="$C_COMPILER" \
           -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
           ../
-    make clang-tidy-albatross_unit_tests-check
+    make clang-tidy-all-ratchet-check
     cd ..
 }
 
