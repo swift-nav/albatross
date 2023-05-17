b4a4d97ee1644b6b41a01c0e104e093e4c85ec71
Matt Peddie <matt.peddie@swift-nav.com>
2023-05-17 15:20:34 +1000

    Increase parameter macro limit from 8 to 20

--- include/albatross/src/core/parameter_macros.hpp
+++ include/albatross/src/core/parameter_macros.hpp
@@ -19,7 +19,9 @@
  */
 
 // Accept any number of args >= N, but expand to just the Nth one.
-#define _GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, N, ...) N
+#define _GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, \
+                     _14, _15, _16, _17, _18, _19, _20, _21, N, ...)    \
+  N
 
 // Define some macros to help us create overrides based on the
 // arity of a for-each-style macro.
@@ -32,10 +34,24 @@
 #define _fe_6(_call, x, ...) _call(x) _fe_5(_call, __VA_ARGS__)
 #define _fe_7(_call, x, ...) _call(x) _fe_6(_call, __VA_ARGS__)
 #define _fe_8(_call, x, ...) _call(x) _fe_7(_call, __VA_ARGS__)
+#define _fe_9(_call, x, ...) _call(x) _fe_8(_call, __VA_ARGS__)
+#define _fe_10(_call, x, ...) _call(x) _fe_9(_call, __VA_ARGS__)
+#define _fe_11(_call, x, ...) _call(x) _fe_10(_call, __VA_ARGS__)
+#define _fe_12(_call, x, ...) _call(x) _fe_11(_call, __VA_ARGS__)
+#define _fe_13(_call, x, ...) _call(x) _fe_12(_call, __VA_ARGS__)
+#define _fe_14(_call, x, ...) _call(x) _fe_13(_call, __VA_ARGS__)
+#define _fe_15(_call, x, ...) _call(x) _fe_14(_call, __VA_ARGS__)
+#define _fe_16(_call, x, ...) _call(x) _fe_15(_call, __VA_ARGS__)
+#define _fe_17(_call, x, ...) _call(x) _fe_16(_call, __VA_ARGS__)
+#define _fe_18(_call, x, ...) _call(x) _fe_17(_call, __VA_ARGS__)
+#define _fe_19(_call, x, ...) _call(x) _fe_18(_call, __VA_ARGS__)
+#define _fe_20(_call, x, ...) _call(x) _fe_19(_call, __VA_ARGS__)
 
-#define CALL_MACRO_X_FOR_EACH(x, ...)                                          \
-  _GET_NTH_ARG("ignored", ##__VA_ARGS__, _fe_8, _fe_7, _fe_6, _fe_5, _fe_4,    \
-               _fe_3, _fe_2, _fe_1, _fe_0)                                     \
+#define CALL_MACRO_X_FOR_EACH(x, ...)                                   \
+  _GET_NTH_ARG("ignored", ##__VA_ARGS__, _fe_20, _fe_19, _fe_18, _fe_17, \
+               _fe_16, _fe_15, _fe_14, _fe_13,                          \
+               _fe_12, _fe_11, _fe_10, _fe_9, _fe_8, _fe_7, _fe_6, _fe_5, \
+               _fe_4, _fe_3, _fe_2, _fe_1, _fe_0)                         \
   (x, ##__VA_ARGS__)
 
 /*
@@ -127,11 +143,74 @@
     _action(x);                                                                \
   } else                                                                       \
     _build_if_7(_cond, _action, __VA_ARGS__)
+#define _build_if_9(_cond, _action, x, ...)                                    \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_8(_cond, _action, __VA_ARGS__)
+#define _build_if_10(_cond, _action, x, ...)                                   \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_9(_cond, _action, __VA_ARGS__)
+#define _build_if_11(_cond, _action, x, ...)                                   \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_10(_cond, _action, __VA_ARGS__)
+#define _build_if_12(_cond, _action, x, ...)                                   \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_11(_cond, _action, __VA_ARGS__)
+#define _build_if_13(_cond, _action, x, ...)                                   \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_12(_cond, _action, __VA_ARGS__)
+#define _build_if_14(_cond, _action, x, ...)                                   \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_13(_cond, _action, __VA_ARGS__)
+#define _build_if_15(_cond, _action, x, ...)                                   \
+  if (_cond(x)) {                                                              \
+    _action(x);                                                                \
+  } else                                                                       \
+    _build_if_14(_cond, _action, __VA_ARGS__)
+#define _build_if_16(_cond, _action, x, ...) \
+  if (_cond(x)) {                            \
+    _action(x);                              \
+  } else                                     \
+    _build_if_15(_cond, _action, __VA_ARGS__)
+#define _build_if_17(_cond, _action, x, ...) \
+  if (_cond(x)) {                            \
+    _action(x);                              \
+  } else                                     \
+    _build_if_16(_cond, _action, __VA_ARGS__)
+#define _build_if_18(_cond, _action, x, ...) \
+  if (_cond(x)) {                            \
+    _action(x);                              \
+  } else                                     \
+    _build_if_17(_cond, _action, __VA_ARGS__)
+#define _build_if_19(_cond, _action, x, ...) \
+  if (_cond(x)) {                            \
+    _action(x);                              \
+  } else                                     \
+    _build_if_18(_cond, _action, __VA_ARGS__)
+#define _build_if_20(_cond, _action, x, ...)    \
+  if (_cond(x)) {                               \
+    _action(x);                                 \
+  } else                                        \
+    _build_if_19(_cond, _action, __VA_ARGS__)
 
-#define BUILD_IF(cond, action, ...)                                            \
-  _GET_NTH_ARG("ignored", ##__VA_ARGS__, _build_if_8, _build_if_7,             \
-               _build_if_6, _build_if_5, _build_if_4, _build_if_3,             \
-               _build_if_2, _build_if_1, _build_if_0)                          \
+#define BUILD_IF(cond, action, ...)                                    \
+  _GET_NTH_ARG("ignored", ##__VA_ARGS__, _build_if_20, _build_if_19,   \
+               _build_if_18, _build_if_17, _build_if_16, _build_if_15, \
+               _build_if_14, _build_if_13, _build_if_12, _build_if_11, \
+               _build_if_10, _build_if_9, _build_if_8, _build_if_7,    \
+               _build_if_6, _build_if_5, _build_if_4, _build_if_3,     \
+               _build_if_2, _build_if_1, _build_if_0)                  \
   (cond, action, ##__VA_ARGS__)
 
 #define SET_PARAM_CONDITION(x) key == #x
