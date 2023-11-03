b91a5cfbdc59bac86dc637851e5ec9093955c345
kleeman <akleeman@gmail.com>
2023-11-03 16:55:29 -0700

    unit tests

--- tests/test_unique.cc
+++ tests/test_unique.cc
@@ -15,9 +15,58 @@
 
 namespace albatross {
 
-TEST(test_unique, test_empty_vector) {
+// Empty input
+
+TEST(test_unique, unique_values_empty) {
   std::vector<int> empty = {};
   EXPECT_EQ(unique_values(empty).size(), 0);
+}
+
+TEST(test_unique, unique_value_empty) {
+  std::vector<int> empty = {};
+  EXPECT_DEATH({ unique_value(empty); }, "Assertion");
+}
+
+TEST(test_unique, unique_values_function_empty) {
+  std::vector<int> empty = {};
+  auto foo = [](const int &x) { return x + 1; };
+  EXPECT_EQ(unique_values(empty, foo).size(), 0);
+}
+
+// Identical inputs
+
+TEST(test_unique, unique_value_identical) {
+  std::vector<int> values = {3, 3, 3};
+  EXPECT_EQ(unique_value(values), 3);
+}
+
+// Mixed input
+
+TEST(test_unique, unique_values_mixed) {
+  std::vector<int> values = {3, 1, 5, 1, 3};
+  std::set<int> expected = {1, 3, 5};
+  EXPECT_EQ(unique_values(values), expected);
+}
+
+TEST(test_unique, unique_value_function_mixed) {
+  std::vector<int> values = {3, 1, 5, 2};
+  auto foo = [](const auto &) -> double { return 4.; };
+  EXPECT_EQ(unique_value(values, foo), 4.);
+}
+
+TEST(test_unique, unique_value_mixed) {
+  std::vector<int> values = {3, 1, 5, 1, 3};
+  EXPECT_DEATH({ unique_value(values); }, "Assertion");
+}
+
+TEST(test_unique, unique_values_function_mixed) {
+  std::vector<int> values = {3, 1, 5, 1, 3};
+  auto foo = [](const int &x) { return x + 1; };
+
+  const auto applied = apply(values, foo);
+  std::set<int> expected(applied.begin(), applied.end());
+
+  EXPECT_EQ(unique_values(values, foo), expected);
 }
 
 } // namespace albatross
