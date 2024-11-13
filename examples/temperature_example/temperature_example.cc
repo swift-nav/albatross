4c951d1baab993f9679def02c1b4b56eaddc013f
LayKuan Loh <laykuan.loh@swift-nav.com>
2024-11-12 16:32:25 -0800

    temperature example

--- examples/temperature_example/temperature_example.cc
+++ examples/temperature_example/temperature_example.cc
@@ -17,8 +17,8 @@
 
 DEFINE_string(input, "/Users/laykuan.loh/swift-nav/albatross/examples/temperature_example/gsod.csv", "path to csv containing input data.");
 DEFINE_string(predict, "/Users/laykuan.loh/swift-nav/albatross/examples/temperature_example/prediction_locations.csv", "path to csv containing prediction locations.");
-DEFINE_string(output, "", "/Users/laykuan.loh/swift-nav/albatross/examples/temperature_example/result.csv");
-DEFINE_string(thin, "1", "thin the data");
+DEFINE_string(output, "/Users/laykuan.loh/swift-nav/albatross/examples/temperature_example/result.csv", "path where predictions will be written in csv.");
+DEFINE_string(thin, "10", "thin the data");
 
 int main(int argc, char *argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
