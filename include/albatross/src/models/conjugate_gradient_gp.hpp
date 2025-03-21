c4b51f75fc4a198924d298fc52c9201deaa8cc93
Matt Peddie <matt.peddie@swift-nav.com>
2025-03-21 13:31:55 +1000

    Options handling and performance comparisons

--- include/albatross/src/models/conjugate_gradient_gp.hpp
+++ include/albatross/src/models/conjugate_gradient_gp.hpp
@@ -15,6 +15,59 @@ namespace albatross {
 template <typename FeatureType, typename Preconditioner>
 struct ConjugateGradientGPFit {};
 
+struct IterativeSolverOptions {
+  // Negative values for these will be ignored.
+  static constexpr const Eigen::Index cDefaultMaxIterations{-1};
+  Eigen::Index max_iterations{cDefaultMaxIterations};
+  static constexpr const double cDefaultTolerance{-1};
+  double tolerance{cDefaultTolerance};
+
+  IterativeSolverOptions() {}
+
+  IterativeSolverOptions(Eigen::Index iterations, double tol)
+      : max_iterations{iterations}, tolerance{tol} {}
+
+  template <typename SolverType>
+  explicit IterativeSolverOptions(SolverType &&solver)
+      : max_iterations{solver.maxIterations()}, tolerance{solver.tolerance()} {}
+
+  template <typename SolverType>
+  void configure_solver(SolverType &solver) const {
+    if (tolerance > 0) {
+      solver.setTolerance(tolerance);
+    }
+
+    if (max_iterations > 0) {
+      solver.setMaxIterations(max_iterations);
+    }
+  }
+};
+
+struct IterativeSolverState {
+  Eigen::ComputationInfo info;
+  Eigen::Index iterations;
+  double error;
+
+  IterativeSolverState() = delete;
+
+  template <typename SolverType>
+  explicit IterativeSolverState(SolverType &&solver)
+      : info{solver.info()},
+        iterations{solver.iterations()}, error{solver.error()} {}
+};
+
+namespace detail {
+
+template <typename SolverType>
+std::shared_ptr<std::decay_t<SolverType>>
+make_shared_solver_with_options(const Eigen::MatrixXd &m,
+                                const IterativeSolverOptions &options) {
+  auto solver = std::make_shared<std::decay_t<SolverType>>(m);
+  options.configure_solver(*solver);
+  return solver;
+}
+} // namespace detail
+
 template <typename FeatureType, typename Preconditioner>
 struct Fit<ConjugateGradientGPFit<FeatureType, Preconditioner>> {
   std::vector<FeatureType> train_features;
@@ -29,13 +82,43 @@ struct Fit<ConjugateGradientGPFit<FeatureType, Preconditioner>> {
   std::shared_ptr<SolverType> solver;
   Eigen::VectorXd information;
 
+  IterativeSolverOptions get_options() const {
+    return IterativeSolverOptions{*solver};
+  }
+
+  IterativeSolverState get_solver_state() const {
+    return IterativeSolverState{*solver};
+  }
+
+  void set_options(const IterativeSolverOptions &options) {
+    options.configure_solver(*solver);
+  }
+
   Fit(const std::vector<FeatureType> &train_features_, Eigen::MatrixXd &&K_ff_,
       const MarginalDistribution &targets_)
       : train_features{train_features_}, K_ff{std::move(K_ff_)},
         // N.B. we give the CG solver a reference to our local member
         // matrix.
-        solver{std::make_shared<SolverType>(K_ff)},
-        information{solver->solve(targets_.mean)} {}
+        solver{std::make_shared<SolverType>(K_ff)}, information{solver->solve(
+                                                        targets_.mean)} {
+    if (solver->info() != Eigen::Success) {
+      information.setConstant(std::numeric_limits<double>::quiet_NaN());
+    }
+  }
+
+  Fit(const std::vector<FeatureType> &train_features_, Eigen::MatrixXd &&K_ff_,
+      const MarginalDistribution &targets_,
+      const IterativeSolverOptions &options)
+      : train_features{train_features_}, K_ff{std::move(K_ff_)},
+        // N.B. we give the CG solver a reference to our local member
+        // matrix.
+        solver{
+            detail::make_shared_solver_with_options<SolverType>(K_ff, options)},
+        information{solver->solve(targets_.mean)} {
+    if (solver->info() != Eigen::Success) {
+      information.setConstant(std::numeric_limits<double>::quiet_NaN());
+    }
+  }
 
   bool operator==(const Fit &other) {
     return std::tie(train_features, K_ff, information) ==
@@ -47,19 +130,30 @@ template <typename CGSolver>
 inline JointDistribution cg_gp_joint_prediction(
     const Eigen::MatrixXd &cross_cov, const Eigen::MatrixXd &prior_cov,
     const Eigen::VectorXd &information, const CGSolver &solver) {
-  return JointDistribution(gp_mean_prediction(cross_cov, information),
-                           prior_cov -
-                               cross_cov.transpose() * solver.solve(cross_cov));
+  JointDistribution ret(gp_mean_prediction(cross_cov, information),
+                        prior_cov -
+                            cross_cov.transpose() * solver.solve(cross_cov));
+  if (solver.info() != Eigen::Success) {
+    ret.covariance.setConstant(std::numeric_limits<double>::quiet_NaN());
+  }
+
+  return ret;
 }
 
 template <typename CGSolver>
 inline MarginalDistribution cg_gp_marginal_prediction(
     const Eigen::MatrixXd &cross_cov, const Eigen::VectorXd &prior_variance,
     const Eigen::VectorXd &information, const CGSolver &solver) {
-  return MarginalDistribution(
+  MarginalDistribution ret(
       gp_mean_prediction(cross_cov, information),
       prior_variance -
           (cross_cov.transpose() * solver.solve(cross_cov)).diagonal());
+  if (solver.info() != Eigen::Success) {
+    ret.covariance.diagonal().setConstant(
+        std::numeric_limits<double>::quiet_NaN());
+  }
+
+  return ret;
 }
 
 template <typename CovFunc, typename MeanFunc, typename Preconditioner>
@@ -75,36 +169,52 @@ public:
   using Base::covariance_function_;
   using Base::mean_function_;
 
-  ConjugateGradientGaussianProcessRegression() : Base() {};
+  ConjugateGradientGaussianProcessRegression() : Base(){};
 
-  template <typename Cov>
+  template <typename Cov,
+            std::enable_if_t<std::is_same<std::decay_t<Cov>, CovFunc>::value,
+                             int> = 0>
   ConjugateGradientGaussianProcessRegression(Cov &&covariance_function)
-      : Base(std::forward<CovFunc>(covariance_function)) {
-    static_assert(
-        std::is_same<std::decay_t<Cov>, CovFunc>::value,
-        "Please construct this with the same covariance it's declared for.");
-  }
+      : Base(std::forward<CovFunc>(covariance_function)) {}
 
-  template <typename Cov>
+  template <typename Cov,
+            std::enable_if_t<std::is_same<std::decay_t<Cov>, CovFunc>::value,
+                             int> = 0>
   ConjugateGradientGaussianProcessRegression(Cov &&covariance_function,
                                              const std::string &model_name)
-      : Base(std::forward<CovFunc>(covariance_function), model_name) {
-    static_assert(
-        std::is_same<std::decay_t<Cov>, CovFunc>::value,
-        "Please construct this with the same covariance it's declared for.");
-  }
+      : Base(std::forward<CovFunc>(covariance_function), model_name) {}
 
-  template <typename Cov, typename Mean>
+  template <typename Cov,
+            std::enable_if_t<std::is_same<std::decay_t<Cov>, CovFunc>::value,
+                             int> = 0>
+  ConjugateGradientGaussianProcessRegression(
+      Cov &&covariance_function, const std::string &model_name,
+      const IterativeSolverOptions &options)
+      : Base(std::forward<CovFunc>(covariance_function), model_name),
+        options_{options} {}
+
+  template <
+      typename Cov, typename Mean,
+      std::enable_if_t<std::is_same<std::decay_t<Cov>, CovFunc>::value &&
+                           std::is_same<std::decay_t<Mean>, MeanFunc>::value,
+                       int> = 0>
   ConjugateGradientGaussianProcessRegression(Cov &&covariance_function,
                                              Mean &&mean_function,
                                              const std::string &model_name)
       : Base(std::forward<CovFunc>(covariance_function),
-             std::forward<MeanFunc>(mean_function), model_name) {
-    static_assert(std::is_same<std::decay_t<Cov>, CovFunc>::value &&
-                      std::is_same<std::decay_t<Mean>, MeanFunc>::value,
-                  "Please construct this with the same covariance and mean "
-                  "it's declared for.");
-  }
+             std::forward<MeanFunc>(mean_function), model_name) {}
+
+  template <
+      typename Cov, typename Mean,
+      std::enable_if_t<std::is_same<std::decay_t<Cov>, CovFunc>::value &&
+                           std::is_same<std::decay_t<Mean>, MeanFunc>::value,
+                       int> = 0>
+  ConjugateGradientGaussianProcessRegression(
+      Cov &&covariance_function, Mean &&mean_function,
+      const std::string &model_name, const IterativeSolverOptions &options)
+      : Base(std::forward<CovFunc>(covariance_function),
+             std::forward<MeanFunc>(mean_function), model_name),
+        options_{options} {}
 
   template <
       typename FeatureType,
@@ -113,13 +223,16 @@ public:
   Fit<ConjugateGradientGPFit<FeatureType, Preconditioner>>
   _fit_impl(const std::vector<FeatureType> &features,
             const MarginalDistribution &targets) const {
-    Eigen::MatrixXd K_ff =
-        covariance_function_(features, Base::threads_.get());
+    Eigen::MatrixXd K_ff = covariance_function_(features, Base::threads_.get());
     MarginalDistribution zero_mean_targets(targets);
     K_ff += targets.covariance;
     mean_function_.remove_from(features, &zero_mean_targets.mean);
+    // Clamp this so that we never try to iterate more than the
+    // theoretical max of CG
+    auto options = options_;
+    options.max_iterations = std::min(options_.max_iterations, K_ff.rows());
     return Fit<ConjugateGradientGPFit<FeatureType, Preconditioner>>{
-        features, std::move(K_ff), std::move(zero_mean_targets)};
+        features, std::move(K_ff), std::move(zero_mean_targets), options};
   }
 
   template <
@@ -130,7 +243,8 @@ public:
           int> = 0>
   JointDistribution _predict_impl(
       const std::vector<FeatureType> &features,
-      const Fit<ConjugateGradientGPFit<FitFeatureType, Preconditioner>> &cg_gp_fit,
+      const Fit<ConjugateGradientGPFit<FitFeatureType, Preconditioner>>
+          &cg_gp_fit,
       PredictTypeIdentity<JointDistribution> &&) const {
     const auto cross_cov =
         covariance_function_(cg_gp_fit.train_features, features);
@@ -182,6 +296,15 @@ public:
     mean_function_.add_to(features, &pred);
     return pred;
   }
+
+  IterativeSolverOptions get_options() const { return options_; }
+
+  void set_options(const IterativeSolverOptions &options) {
+    options_ = options;
+  }
+
+private:
+  IterativeSolverOptions options_{};
 };
 
 // TODO(@peddie): this is known not to have any effect for stationary
@@ -191,24 +314,27 @@ using AlbatrossCGDefaultPreconditioner = Eigen::DiagonalPreconditioner<double>;
 
 template <typename CovFunc,
           typename Preconditioner = AlbatrossCGDefaultPreconditioner>
-auto cg_gp_from_covariance(CovFunc &&covariance_function,
-                           const std::string &model_name,
-                           Preconditioner && = Preconditioner{}) {
+auto cg_gp_from_covariance(
+    CovFunc &&covariance_function, const std::string &model_name,
+    const IterativeSolverOptions &options = IterativeSolverOptions{},
+    Preconditioner && = Preconditioner{}) {
   return ConjugateGradientGaussianProcessRegression<
       typename std::decay<CovFunc>::type, decltype(ZeroMean()), Preconditioner>(
-      std::forward<CovFunc>(covariance_function), model_name);
+      std::forward<CovFunc>(covariance_function), model_name, options);
 }
 
 template <typename CovFunc, typename MeanFunc,
           typename Preconditioner = AlbatrossCGDefaultPreconditioner>
-auto cg_gp_from_mean_and_covariance(CovFunc &&covariance_function,
-                                    MeanFunc &&mean_function,
-                                    const std::string &model_name,
-                                    Preconditioner && = Preconditioner{}) {
+auto cg_gp_from_mean_and_covariance(
+    CovFunc &&covariance_function, MeanFunc &&mean_function,
+    const std::string &model_name,
+    const IterativeSolverOptions &options = IterativeSolverOptions{},
+    Preconditioner && = Preconditioner{}) {
   return ConjugateGradientGaussianProcessRegression<
       typename std::decay<CovFunc>::type, typename std::decay<MeanFunc>::type,
       Preconditioner>(std::forward<CovFunc>(covariance_function),
-                      std::forward<MeanFunc>(mean_function), model_name);
+                      std::forward<MeanFunc>(mean_function), model_name,
+                      options);
 }
 
 } // namespace albatross
--- tests/test_cg_gp.cc
+++ tests/test_cg_gp.cc
@@ -19,29 +19,142 @@
 
 namespace albatross {
 
+std::ostream &operator<<(std::ostream &os, Eigen::ComputationInfo info) {
+  switch (info) {
+  case Eigen::Success:
+    os << "Success";
+    break;
+  case Eigen::NumericalIssue:
+    os << "NumericalIssue";
+    break;
+  case Eigen::NoConvergence:
+    os << "NoConvergence";
+    break;
+  case Eigen::InvalidInput:
+    os << "InvalidInput";
+    break;
+  default:
+    os << "<invalid enum value " << info << ">";
+  };
+  return os;
+}
+
+namespace {
+template <typename FitType>
+void describe_fit(const FitType &fit, std::ostream &os = std::cout) {
+  const auto options = fit.get_options();
+  const auto state = fit.get_solver_state();
+  os << "        Status: " << state.info << std::endl;
+  os << "     Tolerance: " << options.tolerance << std::endl;
+  os << "         Error: " << state.error << std::endl;
+  os << "Max iterations: " << options.max_iterations << std::endl;
+  os << "    Iterations: " << state.iterations << std::endl;
+}
+} // namespace
+
 TEST(TestConjugateGradientGP, TestMean) {
   using CovFunc = SquaredExponential<EuclideanDistance>;
 
   CovFunc covariance(1, 1);
-  auto dataset = make_toy_linear_data();
+  auto dataset = make_toy_linear_data(5, 1, 1.e-3, 5000);
   auto direct = gp_from_covariance(covariance, "direct");
   auto cg = cg_gp_from_covariance(covariance, "cg");
 
+  auto begin = std::chrono::steady_clock::now();
   auto direct_fit = direct.fit(dataset);
+  std::cout << "direct fit: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+  begin = std::chrono::steady_clock::now();
   auto cg_fit = cg.fit(dataset);
+  std::cout << "cg fit: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
 
-  auto test_features = linspace(0.01, 9.9, 11);
+  describe_fit(cg_fit.get_fit());
+  auto test_features = linspace(0.01, 9.9, 100);
 
-  auto direct_pred = direct_fit.predict_with_measurement_noise(test_features).joint();
+  begin = std::chrono::steady_clock::now();
+  auto direct_pred =
+      direct_fit.predict_with_measurement_noise(test_features).joint();
+  std::cout << "direct predict: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+
+  begin = std::chrono::steady_clock::now();
   auto cg_pred = cg_fit.predict_with_measurement_noise(test_features).joint();
+  std::cout << "cg predict: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+  describe_fit(cg_fit.get_fit());
+
+  EXPECT_TRUE(cg_pred.mean.array().isFinite().all());
+  EXPECT_TRUE(cg_pred.covariance.array().isFinite().all());
 
   double mean_error = (direct_pred.mean - cg_pred.mean).norm();
   EXPECT_LT(mean_error, 1.e-9);
 
-  double cov_distance =
-      albatross::distance::wasserstein_2(direct_pred, cg_pred);
+  double cov_error = (direct_pred.covariance - cg_pred.covariance).norm();
+  EXPECT_LT(cov_error, 1.e-9);
 
-  EXPECT_LT(cov_distance, 1.e-9);
+  begin = std::chrono::steady_clock::now();
+  auto direct_marginal =
+      direct_fit.predict_with_measurement_noise(test_features).marginal();
+  std::cout << "direct marginal: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+
+  begin = std::chrono::steady_clock::now();
+  auto cg_marginal =
+      cg_fit.predict_with_measurement_noise(test_features).marginal();
+  std::cout << "cg marginal: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+  describe_fit(cg_fit.get_fit());
+  double marginal_error = (direct_marginal.covariance.diagonal() -
+                           cg_marginal.covariance.diagonal())
+                              .norm();
+  EXPECT_LT(marginal_error, 1.e-9);
+
+  begin = std::chrono::steady_clock::now();
+  auto direct_mean =
+      direct_fit.predict_with_measurement_noise(test_features).mean();
+  std::cout << "direct mean: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+
+  begin = std::chrono::steady_clock::now();
+  auto cg_mean = cg_fit.predict_with_measurement_noise(test_features).mean();
+  std::cout << "cg mean: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::steady_clock::now() - begin)
+                       .count() /
+                   1.e9
+            << std::endl;
+  describe_fit(cg_fit.get_fit());
+  double mean_predict_error = (direct_mean - cg_mean).norm();
+  EXPECT_LT(mean_predict_error, 1.e-9);
 }
 
 } // namespace albatross
