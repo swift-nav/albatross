5a595a244c268a33fd15bdc492571da7c9758707
Matt Peddie <matt.peddie@swift-nav.com>
2025-10-15 21:47:51 +1000

    Support heterogeneous comparison in map utils
    Previously map utilities were constrained to support only map types
    with two template parameters (key and value), preventing use of
    `map_contains()` and friends with things like `std::map` with a custom
    comparator.
    
    This commit generalises the template signatures of the map utilities
    to support such maps and adds SFINAE detection for `find()` with any
    arbitrary key type where appropriate, thereby supporting transparent
    heterogeneous access in `map_contains()` and `map_at_or()`.

--- tests/test_map_utils.cc
+++ tests/test_map_utils.cc
@@ -11,6 +11,7 @@
  */
 
 #include <albatross/Common>
+#include <charconv>
 #include <gtest/gtest.h>
 
 namespace albatross {
@@ -28,6 +29,47 @@ TEST(test_map_utils, test_map_contains) {
   // Removing a value from the map should cause a False.
   test_map.erase(3);
   EXPECT_FALSE(map_contains(test_map, 3));
+}
+
+namespace {
+
+int from_string_view(std::string_view s) {
+  int si{};
+  auto [ptr, ec] = std::from_chars(s.data(), s.data() + s.size(), si);
+  ALBATROSS_ASSERT(ec == std::errc{} && ptr == s.data() + s.size() &&
+                   "Invalid number string");
+  return si;
+}
+
+struct CompareString {
+  using is_transparent = void;
+  inline bool operator()(std::string_view s, int i) const {
+    return from_string_view(s) < i;
+  }
+
+  inline bool operator()(int i, std::string_view s) const {
+    return i < from_string_view(s);
+  }
+
+  inline bool operator()(int i, int j) const { return i < j; }
+};
+
+} // namespace
+
+TEST(test_map_utils, test_map_contains_heterogeneous) {
+  std::map<int, int, CompareString> test_map = {{1, 2}, {2, 3}, {3, 4}, {6, 7}};
+
+  // map_contain should return true for all keys.
+  for (const auto &pair : test_map) {
+    EXPECT_TRUE(map_contains(test_map, pair.first));
+  }
+  using std::literals::string_view_literals::operator""sv;
+  // But should return false for ones not included.
+  EXPECT_FALSE(map_contains(test_map, "-1"sv));
+
+  // Removing a value from the map should cause a False.
+  test_map.erase(3);
+  EXPECT_FALSE(map_contains(test_map, "3"));
 }
 
 TEST(test_map_utils, map_at_or) {
@@ -57,6 +99,21 @@ bool is_in_vector(const std::vector<int> &vector, int key) {
 
 TEST(test_map_utils, map_keys) {
   const std::map<int, int> test_map = {{1, 2}, {2, 3}, {3, 4}, {6, 7}};
+  const std::vector<int> keys = map_keys(test_map);
+
+  // Any key in the map should be in the vector of keys.
+  for (const auto &pair : test_map) {
+    EXPECT_TRUE(is_in_vector(keys, pair.first));
+  }
+  // A key not in the map should not be in the vector
+  EXPECT_FALSE(is_in_vector(keys, -1));
+  // The length of the map and vector should be the same
+  EXPECT_EQ(static_cast<int>(keys.size()), static_cast<int>(test_map.size()));
+}
+
+TEST(test_map_utils, map_keys_heterogeneous) {
+  const std::map<int, int, CompareString> test_map = {
+      {1, 2}, {2, 3}, {3, 4}, {6, 7}};
   const std::vector<int> keys = map_keys(test_map);
 
   // Any key in the map should be in the vector of keys.
